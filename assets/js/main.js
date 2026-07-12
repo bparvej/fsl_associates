25b8f762019cccd3ea415cc0aa2c91d02eacaf30
Parvej Bapary <bparvej@gmail.com>
2026-07-12 22:27:45 +0600

    direct ph call button added

--- assets/js/main.js
+++ assets/js/main.js
@@ -248,6 +248,16 @@ function setupWhatsAppButton() {
     document.body.appendChild(whatsappBtn);
 }
 
+function setupCallButton() {
+    const callBtn = document.createElement('a');
+    callBtn.href = 'tel:01921303050';
+    callBtn.className = 'call-float';
+    callBtn.id = 'call-float';
+    callBtn.setAttribute('aria-label', 'Call Us');
+    callBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white"><path d="M20.01 15.38c-1.23 0-2.42-.2-3.53-.56a.977.977 0 0 0-1.01.24l-1.57 1.97c-2.83-1.35-5.48-3.9-6.89-6.83l1.95-1.66c.27-.28.35-.67.24-1.02-.37-1.11-.56-2.3-.56-3.53 0-.54-.45-.99-.99-.99H4.19C3.65 3 3 3.24 3 3.99 3 13.28 10.73 21 20.01 21c.71 0 .99-.63.99-1.18v-3.45c0-.54-.45-.99-.99-.99z"/></svg>';
+    document.body.appendChild(callBtn);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     loadFragment('#header-placeholder', 'header.html', fallbackHeaderHTML);
     loadFragment('#footer-placeholder', 'footer.html', fallbackFooterHTML);
@@ -255,6 +265,7 @@ document.addEventListener('DOMContentLoaded', () => {
     setupActiveMenu();
     setupFloatingButton();
     setupWhatsAppButton();
+    setupCallButton();
     initCounterAnimation();
     initAppointmentTabs();
 
